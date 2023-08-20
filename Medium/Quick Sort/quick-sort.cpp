62388300a46afe75f62433a6134483523d042f79
Tanvi Shingi <128184003+tanvii-17@users.noreply.github.com>
2023-08-20 14:45:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Quick Sort/quick-sort.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low >= high)
+            return;
+        int pivotIndex = partition(arr, low, high);
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       int pivot = low;
+        int i=low;
+        int j=high;
+        
+        while(i<j)
+        {
+            while(arr[i] <= arr[pivot])
+                i++;
+            while(arr[j] > arr[pivot])
+                j--;
+            if(i < j)
+                swap(arr[j], arr[i]);
+        }
+        swap(arr[j], arr[pivot]);
+        return j;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
