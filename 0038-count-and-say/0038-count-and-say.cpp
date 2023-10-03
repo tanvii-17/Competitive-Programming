998e3a6d49fc01cc30de67ef8762369d1bfaaeb0
Tanvi Shingi <128184003+tanvii-17@users.noreply.github.com>
2023-10-03 20:35:30 +0530

    Time: 6 ms (64.5%), Space: 7.2 MB (39.06%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ans = "1";
+        int i = 1;
+        while (i < n) {
+            string cur = "";
+            for (int j = 0; j < ans.size(); j++) {
+                int count = 1;
+                while (j + 1 < ans.size() && ans[j] == ans[j + 1]) {
+                    count++;
+                    j++;
+                }    
+                cur += to_string(count) + ans[j];    
+            } 
+            ans = cur; 
+            i++;
+        }
+        return ans;
+    }
+};
