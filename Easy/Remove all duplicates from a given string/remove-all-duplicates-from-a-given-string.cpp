5ac51a98c7d6a6e1f6fd83be82f0242ace819077
Tanvi Shingi <128184003+tanvii-17@users.noreply.github.com>
2023-10-04 14:13:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove all duplicates from a given string/remove-all-duplicates-from-a-given-string.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    string ans = "";
+	    set<char>st;
+	    
+	    for(int i=0; i<str.size(); i++){
+	        if(st.find(str[i]) == st.end()){
+	            st.insert(str[i]);
+	            ans +=str[i];
+	        }
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
