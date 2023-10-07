a56989f2fe752172e0b23f8077acc25bc984e596
Tanvi Shingi <128184003+tanvii-17@users.noreply.github.com>
2023-10-07 21:04:56 +0530

    Time: 19 ms (81.83%), Space: 71.7 MB (19.96%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    void insertAtTail(ListNode* &head, ListNode* &tail, int val){
+        ListNode* temp = new ListNode(val);
+        //empty list
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+        }
+        else{
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    ListNode* add(ListNode* l1, ListNode* l2){
+        int carry = 0;
+        ListNode* ansHead = NULL;
+        ListNode* ansTail = NULL;
+        
+        while(l1 != NULL && l2 != NULL){
+        int sum = carry + l1->val + l2->val;
+        int digit = sum % 10;
+        insertAtTail(ansHead, ansTail, digit);
+        carry = sum / 10;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+        while(l1 != NULL){
+            int sum = carry + l1->val;
+            int digit = sum % 10;
+            insertAtTail(ansHead, ansTail, digit);
+            carry = sum / 10;
+            l1 = l1->next;
+        }
+        while(l2 != NULL){
+            int sum = carry + l2->val;
+            int digit = sum % 10;
+            insertAtTail(ansHead, ansTail, digit);
+            carry = sum / 10;
+            l2 = l2->next;
+        }
+        while(carry != 0){
+            int sum = carry;
+            int digit = sum % 10;
+            insertAtTail(ansHead, ansTail, digit);
+            carry = sum / 10;
+        }
+        return ansHead;
+    }
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans = add(l1, l2);
+        return ans;
+    }
+};
