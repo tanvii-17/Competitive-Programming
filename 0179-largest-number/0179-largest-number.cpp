7bf251cbb8fcea4fc48588c2bb4cff9f2bdd12b5
Tanvi Shingi <128184003+tanvii-17@users.noreply.github.com>
2023-10-07 19:36:44 +0530

    Time: 3 ms (95.3%), Space: 11.4 MB (59.55%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,16 @@
+ bool compare(int a, int b){
+        return to_string(a) + to_string(b) > to_string(b) + to_string(a);
+    }
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        string ans = "";
+        sort(nums.begin(), nums.end(), compare);
+        for(int i=0; i<nums.size(); i++){
+            ans += to_string(nums[i]);
+            if(ans[0] == '0') return "0";
+            
+        }
+        return ans;
+    }
+};
