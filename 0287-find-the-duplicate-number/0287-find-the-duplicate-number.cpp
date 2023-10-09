f7b64bbcc58f5e0083b68dca462156edf40ab2e8
Tanvi Shingi <128184003+tanvii-17@users.noreply.github.com>
2023-10-09 12:43:33 +0530

    Time: 143 ms (18.42%), Space: 84.4 MB (14.62%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int>s;
+        for(auto num:nums){
+            if(s.find(num) != s.end()){
+                return num;
+            }
+            s.insert(num);
+        }
+        return -1;
+    }
+};
