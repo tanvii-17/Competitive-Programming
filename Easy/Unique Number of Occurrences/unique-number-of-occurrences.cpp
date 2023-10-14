8f5a39f86de54ef08505824d9dd824920c720a01
Tanvi Shingi <128184003+tanvii-17@users.noreply.github.com>
2023-10-14 19:46:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Unique Number of Occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {
+        unordered_map<int,int>mpp;
+        set<int>st;
+        
+        for(int i=0; i<n; i++){
+            mpp[arr[i]]++;
+        }
+        for(auto num:mpp){
+            st.insert(num.second);
+        }
+        return mpp.size() == st.size();
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
